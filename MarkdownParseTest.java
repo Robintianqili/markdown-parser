83ab6062b60a3e7824468c296aa48a36f285f6c5
Robintianqili <til024@ucsd.edu>
2022-06-03 18:28:39 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,33 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 public class MarkdownParseTest {
 
     @Test
-    public void test () {
-        assertEquals(2, 1 + 1);
+    public void testFile1() throws IOException {
+        String contents= Files.readString(Path.of("Snippet1.md"));
+        List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
+        assertEquals(expect,MarkdownParse.getLinks(contents));
     }
 
+    @Test
+    public void testFile2() throws IOException {
+        String contents= Files.readString(Path.of("Snippet2.md"));
+        List<String> expect = List.of("a.com(())", "example.com");
+        assertEquals(expect,MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSingleImage() throws IOException {
+        String contents= Files.readString(Path.of("Snippet3.md"));
+        List<String> expect = List.of("https://www.twitter.com", 
+        "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule",
+         "https://cse.ucsd.edu/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
 }
